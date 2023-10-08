307d773891509b93ecd080a6da26322a4a481a15
wyf <1328988774@qq.com>
2023-10-08 15:30:04 +0800

    初始代码文件夹

---
+++ build_support/run_executable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# run all executable files 
+for file in ` ls `
+    do 
+        if [[ -f ${file} && -x ${file} ]]; then
+            echo "In debug mode run ${file}......"
+            ./$file
+            if [[ $? -ne 0 ]]; then
+                echo "In debug mode run ${file} failed!"
+                exit 1
+            fi
+        fi
+    done
+
+echo "In debug mode all tests passed!"
---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[3] = {0};
+    for (int i = 0; i < 3; i++) {
+        cout << "wuhu";
+        cout << a[i];
+    }
+    cout << "Hello World";
+}
